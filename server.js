355d20d52c833b876bbf399d51662987bcc6838f
Tasneem-Alabed <tsneemabedd@gmail.com>
2023-05-01 16:55:38 +0000

    solve lab 13

---
+++ server.js
@@ -0,0 +1,48 @@
+ 
+const express = require('express');
+ 
+
+const server = express();
+
+ const PORT=3000;
+
+ server.get('/',(req,res)=>{
+    
+    function GetDataJson (   titleValur ,  pathVal ,  obverVal){
+ 
+this.titleValur=titleValur;
+ 
+this.pathVal=pathVal;
+ 
+this.obverVal=obverVal;
+    
+    }
+ let move= new  GetDataJson(  Object.values(dataJson)[0] ,
+     Object.values(dataJson)[4],  
+   Object.values(dataJson)[7] );
+
+   res.send(move);
+ })
+
+ server.get('/favorite',(req,res)=>{
+    res.send("Welcome to Favorite Page");
+ })
+ server.get('/favorite1',(req,res)=>{
+    res.status(500).send("status: 500"+
+    "responseText:"+ "Sorry, something went wrong");
+
+ })
+ const dataJson = require("./MovieData/data.json");
+  
+
+
+ 
+ server.get('*',(req,res)=>{
+    res.status(404).send("page not found error");
+
+ })
+ 
+
+ server.listen(PORT,()=>{
+    console.log("server");
+ })
